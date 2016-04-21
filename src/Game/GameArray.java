bca5b5f33424982de5fe33c6f943352233cd85b8
bartek <klys.bartosz@gmail.com>
2016-04-21 20:57:28 +0200

    Repaired a bug with win/draw mismatch on the ninth move.

--- src/Game/GameArray.java
+++ src/Game/GameArray.java
@@ -23,29 +23,31 @@ public class GameArray {
     }
 
     private void winCheck() {
-        if(gameArray[0] != 0 && gameArray[1] != 0 && gameArray[2] != 0 && gameArray[3] != 0 && gameArray[4] != 0 && gameArray[5] != 0 && gameArray[6] != 0 && gameArray[7] != 0 && gameArray[8] != 0){
-            JOptionPane.showMessageDialog(null, "Draw!");
-            board.disableButtons();
-        }
-        else if ((gameArray[0] == 1 && gameArray[1] == 1 && gameArray[2] == 1) ||
-                (gameArray[3] == 1 && gameArray[4] == 1 && gameArray[5] == 1) ||
-                (gameArray[6] == 1 && gameArray[7] == 1 && gameArray[8] == 1) ||
-                (gameArray[0] == 1 && gameArray[3] == 1 && gameArray[6] == 1) ||
-                (gameArray[1] == 1 && gameArray[4] == 1 && gameArray[7] == 1) ||
-                (gameArray[2] == 1 && gameArray[5] == 1 && gameArray[8] == 1) ||
-                (gameArray[0] == 1 && gameArray[4] == 1 && gameArray[8] == 1) ||
-                (gameArray[2] == 1 && gameArray[4] == 1 && gameArray[6] == 1)) {
+        if ((gameArray[0] == 1 && gameArray[1] == 1 && gameArray[2] == 1) ||
+        (gameArray[3] == 1 && gameArray[4] == 1 && gameArray[5] == 1) ||
+        (gameArray[6] == 1 && gameArray[7] == 1 && gameArray[8] == 1) ||
+        (gameArray[0] == 1 && gameArray[3] == 1 && gameArray[6] == 1) ||
+        (gameArray[1] == 1 && gameArray[4] == 1 && gameArray[7] == 1) ||
+        (gameArray[2] == 1 && gameArray[5] == 1 && gameArray[8] == 1) ||
+        (gameArray[0] == 1 && gameArray[4] == 1 && gameArray[8] == 1) ||
+        (gameArray[2] == 1 && gameArray[4] == 1 && gameArray[6] == 1)) {
             JOptionPane.showMessageDialog(null, "Player X is a winner!");
             board.disableButtons();
         } else if ((gameArray[0] == 2 && gameArray[1] == 2 && gameArray[2] == 2) ||
-                (gameArray[3] == 2 && gameArray[4] == 2 && gameArray[5] == 2) ||
-                (gameArray[6] == 2 && gameArray[7] == 2 && gameArray[8] == 2) ||
-                (gameArray[0] == 2 && gameArray[3] == 2 && gameArray[6] == 2) ||
-                (gameArray[1] == 2 && gameArray[4] == 2 && gameArray[7] == 2) ||
-                (gameArray[2] == 2 && gameArray[5] == 2 && gameArray[8] == 2) ||
-                (gameArray[0] == 2 && gameArray[4] == 2 && gameArray[8] == 2) ||
-                (gameArray[2] == 2 && gameArray[4] == 2 && gameArray[6] == 2)) {
+        (gameArray[3] == 2 && gameArray[4] == 2 && gameArray[5] == 2) ||
+        (gameArray[6] == 2 && gameArray[7] == 2 && gameArray[8] == 2) ||
+        (gameArray[0] == 2 && gameArray[3] == 2 && gameArray[6] == 2) ||
+        (gameArray[1] == 2 && gameArray[4] == 2 && gameArray[7] == 2) ||
+        (gameArray[2] == 2 && gameArray[5] == 2 && gameArray[8] == 2) ||
+        (gameArray[0] == 2 && gameArray[4] == 2 && gameArray[8] == 2) ||
+        (gameArray[2] == 2 && gameArray[4] == 2 && gameArray[6] == 2)) {
             JOptionPane.showMessageDialog(null, "Player O is a winner!");
+            board.disableButtons();
+        }
+        else if (gameArray[0] != 0 && gameArray[1] != 0 && gameArray[2] != 0 &&
+                gameArray[3] != 0 && gameArray[4] != 0 && gameArray[5] != 0 &&
+                gameArray[6] != 0 && gameArray[7] != 0 && gameArray[8] != 0){
+            JOptionPane.showMessageDialog(null, "Draw!");
             board.disableButtons();
         }
     }
